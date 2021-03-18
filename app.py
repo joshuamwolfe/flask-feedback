fed8922511dcabc5f8cabb70aaa4361987af604f
Joshua Wolfe <joshuamwolfe@gmail.com>
2021-03-18 15:53:03 -0400

    setting up project

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, url_for, render_template, redirect, flash, jsonify, req
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
+
 # from forms import AddPetForm, EditPetForm
 
 app = Flask(__name__)
@@ -20,21 +21,21 @@ connect_db(app)
 # Make routes for the following:
 
 # GET /
-    # Redirect to /register.
+# Redirect to /register.
 
 # GET /register
-    # Show a form that when submitted will register/create a user. This form should accept a username, password, email, first_name, and last_name.
-    # Make sure you are using WTForms and that your password input hides the characters that the user is typing!
+# Show a form that when submitted will register/create a user. This form should accept a username, password, email, first_name, and last_name.
+# Make sure you are using WTForms and that your password input hides the characters that the user is typing!
 
 # POST /register
-    # Process the registration form by adding a new user. Then redirect to /secret
+# Process the registration form by adding a new user. Then redirect to /secret
 
 # GET /login
-    # Show a form that when submitted will login a user. This form should accept a username and a password.
-    # Make sure you are using WTForms and that your password input hides the characters that the user is typing!
+# Show a form that when submitted will login a user. This form should accept a username and a password.
+# Make sure you are using WTForms and that your password input hides the characters that the user is typing!
 
 # POST /login
-    # Process the login form, ensuring the user is authenticated and going to /secret if so.
+# Process the login form, ensuring the user is authenticated and going to /secret if so.
 
 # GET /secret
-    # Return the text “You made it!” (don’t worry, we’ll get rid of this soon)
+# Return the text “You made it!” (don’t worry, we’ll get rid of this soon)
---
+++ notes.md
@@ -0,0 +1,24 @@
+# Hashing Notes
+
+---
+## Packages to Install
+
+- bcrypt package
+
+~~~python
+pip install bcrypt
+~~~  
+
+- flask bcrypt wrapper
+
+~~~python
+pip install flask_bcrypt
+~~~
+
+## Keep In Mind
+
+- Only logged in user should see the "/secret" route
+  - refer to video: "Keeping a user logged"
+- Most answers can be found in demo code and hand outs
+
+
